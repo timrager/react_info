eaefb35f045fe56bd0ee74c78ca1a8c6728b47ec
Tim Rager <tim.rager@gmail.com>
2021-11-14 11:05:24 -0800

    added html

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className="App">
-      <header>
-        <p>Test</p>
-      </header>
+    <div>
+      <img src="/react-logo.png" width="40" alt="logo"/>
+      <h1>Fun facts about React!</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
     </div>
   );
 }
