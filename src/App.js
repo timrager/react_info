0c5610241b9e04e2578e91df56dedfe1724bb829
Tim Rager <tim.rager@gmail.com>
2021-11-14 12:19:15 -0800

    added components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import './style.css';
 
 const App = () => {
   return (
     <div>
-      <img src="/react-logo.png" width="40" alt="logo"/>
-      <h1>Fun facts about React!</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
