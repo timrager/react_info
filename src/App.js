34361cc1e91222dffca6e7eccc5c2f1fd6e568cc
Tim Rager <tim.rager@gmail.com>
2021-11-14 09:33:06 -0800

    skeleton

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+        <p>Test</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
